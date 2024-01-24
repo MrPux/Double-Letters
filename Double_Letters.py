d35f88122f450bc6c9d19d85fa1f6fe08783bac4
MrPuxe <anthondeleonsketch@gmail.com>
2024-01-24 10:46:12 -0500

    Commit with instructions

---
+++ Double_Letters.py
@@ -0,0 +1,8 @@
+#   Analyze a string to check if it contains two of the same letter in a row.
+#   E.g, "Hello" has 'l' twice in a row, 
+#   while the string "nono" does not have two identical letters in a row.
+
+#   Function must return 'True' if there are two identical letters in a row in the string,
+    #and 'False' otherwise
+#4.5 
+
